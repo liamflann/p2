dde3d26c2377f0bf7dc86f6b7dcf87b6170d8f57
Liam Flannagan <William.Flannagan@UCDenver.edu>
2018-05-02 21:47:03 -0600

    incorporates the update from junk3

--- playgame.py
+++ playgame.py
@@ -14,6 +14,7 @@ Hope to add Hangman to it later.
 
 #Collection of games project.
 
+import numpy as np
 import random
 import pandas as pd
 import csv
@@ -72,8 +73,7 @@ def differentgame():
 
 def hangman():
 
-    level = input("Which level do you want to play?", '\n',
-                  "Easy, medium, or hard?")
+    level = input("Which level do you want to play? Easy, medium, or hard? ")
     level= level.lower()
     
     if level == "easy":
@@ -107,13 +107,86 @@ def hangman():
         
     for char in secretWord:
         sys.stdout.write("_ ")
+
+    secretWord = secretWord.lower()
+    guessedWord = "_" * len(secretWord)
+    guessedWord = list(guessedWord)
+    word = list(secretWord)
+    usedLetters = []
+    counter = 0
+    def wrongGuess(counter):
+        counter = counter + 1
+        print(counter)
+        getGuess()
     
+    def gotWord(guessedWord):
+        gotIt = "_"*len(secretWord)
+        if word == list(gotIt):
+            print("You won!")
+            print("Want to play again? Y/N")
+            answer = input()
+            answer = answer.lower()
+            while (answer == 'y'):
+                hangman()
+            if answer=='n':
+                differentgame()
+        else:
+            getGuess()
+    #Ask user for a letter
+    def getGuess():
+        guess = input("What letter do you guess? ")
+        checkforLetter(guess, counter)
+        return
+    
+    def morethanoneLetter(secretWord, guess):
+        print("The letter occurs more than once.")
+        c = secretWord.count(guess) -1
+        index = word.index(guess)
+        guessedWord[index]=guess
+        word[index]="_"
+        print(' '.join(guessedWord))
+        print("Please type the letter THEN press enter {} times".format(c))
+        i = 0
+        while i <= c:
+            extra = input()
+            index = word.index(extra)
+            guessedWord[index]=extra
+            word[index]="_"
+            print(' '.join(guessedWord))
+            i = i +1
+            j = c - i
+            if j > 0:
+                print("Thanks, enter the letter {} more time(s)".format(j))
+            if i == c:
+                break
+        gotWord(guessedWord)
+        
+    def oneLetter(guess):
+        index = word.index(guess)
+        guessedWord[index]=guess
+        word[index]="_"
+        print(' '.join(guessedWord))
+        gotWord(guessedWord)
+        
+    
+    def checkforLetter(guess, counter):
+        if (secretWord.find(guess)!=-1):
+            print("The letter is there.")
+            if (secretWord.count(guess) > 1):
+                morethanoneLetter(secretWord,guess)
+            else:
+                oneLetter(guess)
+        else:
+            print("Sorry, the letter is not there.")
+            getGuess()
+            
+    getGuess()
 
 
     
         
 
-    thanksforplaying()
+    #thanksforplaying()
     return
 
 
@@ -217,4 +290,8 @@ def guess():
     return
 
 #The main function of this program.
-gamechoice()
+if __name__ == '__main__':
+   gamechoice()
+   input("Press ENTER to exit")
+   
+
